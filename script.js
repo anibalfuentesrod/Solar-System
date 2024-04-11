e527cb92271b26c2aa79894fb04aeba3e3f67c10
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-04-11 09:55:46 -0400

    pushing the new files

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const starsContainer = document.getElementById("stars-container");
+    const numStars = 800;
+
+    for (let i = 0; i < numStars; i++) {
+        const star = document.createElement("div");
+        star.classList.add("star");
+        starsContainer.appendChild(star);
+        positionStar(star);
+    }
+});
+
+function positionStar(star) {
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    const size = Math.random() * 5;
+
+    star.style.left = x + "px";
+    star.style.top = y + "px";
+    star.style.width = size + "px";
+    star.style.height = size + "px";
+}
+
